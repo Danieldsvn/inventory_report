81f78a9bba99a7a6d4577c6d2c4345cc8d1547dd
Daniel Saraiva <danieldsvn@gmail.com>
2023-03-03 16:44:04 -0300

    Implementa teste da classe Product(Criação da instância)

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
 
 # Iniciando projeto
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        "1",
+        "Torradeira",
+        "Arno",
+        "03/03/2023",
+        "03/06/2023",
+        121314,
+        "instruções",
+    )
+
+    assert product.id == "1"
+    assert product.nome_do_produto == "Torradeira"
+    assert product.nome_da_empresa == "Arno"
+    assert product.data_de_fabricacao == "03/03/2023"
+    assert product.data_de_validade == "03/06/2023"
+    assert product.numero_de_serie == 121314
+    assert product.instrucoes_de_armazenamento == "instruções"
