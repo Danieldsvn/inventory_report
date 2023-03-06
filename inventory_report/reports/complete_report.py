8afda900b867a717fdac58d525a26f7c74d2a517
Daniel Saraiva <danieldsvn@gmail.com>
2023-03-06 12:24:25 -0300

    Implementa método estático generate da Classe Complete Report e herda de Simple Report. Precisa alterar a ordem das empresas listadas no conjunto 'qty_produtos'

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,32 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(companies):
+        print("companies ->", companies)
+        products_company = CompleteReport.products_per_company(companies)
+        return f"""
+        {SimpleReport.generate(companies)}
+        {products_company}
+        """
+
+    def products_per_company(companies):
+        # print("'products_pr_company' foi chamada")
+        nome_das_empresas = []
+        qty_produtos = set()
+        string = "Produtos estocados por empresa:\n"
+        for company in companies:
+            nome_das_empresas.append(company["nome_da_empresa"])
+        print("nome_das_empresas ->", nome_das_empresas)
+        # Precisa alterar a ordem do nomes das empresas em "qty_produtos"
+        for company in nome_das_empresas:
+            company_frequency = nome_das_empresas.count(company)
+            company_frequency_element = (company, company_frequency)
+            qty_produtos.add(company_frequency_element)
+        print("qty_produtos", qty_produtos)
+        for qty in qty_produtos:
+            # print(string)
+            string = string + f"- {qty[0]}: {qty[1]}\n"
+        print("string", string)
+        return string
